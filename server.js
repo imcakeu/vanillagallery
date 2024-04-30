0128235e9d128c54833555aef83f0ec573e2ce35
Eduard Diconi <33260932+imcakeu@users.noreply.github.com>
2024-04-30 16:50:00 +0200

    Chapter 5
    dated 19/03/2024

--- server.js
+++ server.js
@@ -2,47 +2,70 @@
 const http = require("http");
 const fs = require("fs");
 
-function sendImage(imagePath, contentType, res) {
-    fs.readFile(imagePath, (err, data) => {
-        if (err) {
-            console.error(err);
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("Internal Server Error");
-        } else {
-            res.writeHead(200, { "Content-Type": contentType });
-            res.end(data);
-        }
-    });
-}
+// Variables 
+const imageCount = 14;
+const descriptions = [];
+initializeDescriptions();
+function initializeDescriptions() { for(let i=0; i<=imageCount; i++) descriptions[i] = ""; }
 
 const host = 'localhost';
 const port = 8080;
 const server = http.createServer();
 server.on("request", (req, res) => {
-     if (req.url.startsWith('/image/')) {
-          try {
-               res.end(fs.readFileSync('.'+req.url));
-          } 
-          catch (err) {
-              errorHandler(err);
+     // Accès ressources
+     if(req.method === 'GET') {
+          if (req.url.startsWith('/image/')) {
+               try {
+                    res.end(fs.readFileSync('.'+req.url));
+               } 
+               catch (err) {
+                   errorHandler(err);
+               }
+          }
+          else if(req.url.startsWith("/imageviewer/")){
+               const image = req.url.slice(18, req.url.length-4);
+               res.end(pageViewer(image));
+          }
+          else if(req.url == "/style") {
+               res.end(fs.readFileSync("./style.css", "utf-8"));
+          }
+          else if(req.url == "/gallery"){
+               res.end(pageGallery());
+          }
+          else if(req.url == "/image-description"){
+               res.end(fs.readFileSync("./image-description.html", "utf-8"));
+          }
+          else if(req.url == "/"){
+               res.end(fs.readFileSync("./index.html", "utf-8"));
+          }   
+          else{
+               res.end(page404("page not found"));
           }
      }
-     else if(req.url.startsWith("/imageviewer/")){
-          const image = req.url.slice(18, req.url.length-4);
-          res.end(pageViewer(image));
+     // Requête de publication de commentaire
+     else if (req.method === "POST" && req.url === "/description-image") {
+          let donnees;;
+          req.on("data", (dataChunk) => {
+              donnees += dataChunk.toString();
+          });
+          req.on("end", () => {
+               const paramValeur = donnees.split("&");
+               const imageNumber = paramValeur[0].split("=")[1];
+               const description = paramValeur[1].split("=")[1];
+
+               if(description != ""){
+                    descriptions[imageNumber] += "<b>[" + randomUsername() + "]:</b> " + decodeURIComponent(description) + "<br>";
+                    res.statusCode = 302;
+                    res.setHeader('Location', '/imageviewer/image' + imageNumber + ".jpg");
+                    res.end();
+                    // res.statusCode = 200;
+                    // res.end(pageDescImage(decodeURIComponent(description), imageNumber));
+               }
+               else{
+                    res.end(page404("can't send an empty comment!"));
+               }
+          });
      }
-     else if(req.url == "/favicon.ico"){
-          res.end(fs.readFileSync("./image/favicon.ico"));
-     }
-     else if(req.url == "/style") {
-          res.end(fs.readFileSync("./style.css", "utf-8"));
-     }
-     else if(req.url == "/gallery"){
-          res.end(pageGallery());
-     }
-     else if(req.url == "/"){
-          res.end(fs.readFileSync("./index.html", "utf-8"));
-     }   
      else{
           res.end(page404("page not found"));
      }
@@ -50,14 +73,17 @@ server.on("request", (req, res) => {
 
 server.listen(port, host, () => {
      console.log(`Server running at http://${host}:${port}/`);
- });
+});
 
+// Gallerie/mur de toutes les images disponibles
+// Possibilité de cliquer sur les images pour le lancer dans le pageViewer/imageviewer
 function pageGallery(){
      let files = fs.readdirSync('./image');
      let sFiles = files.filter(f => f.endsWith('_small.jpg'));
 
      let html = '<!DOCTYPE html><html lang="fr">';
      html += '<meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">'
+     html += '<link rel="icon" href="image/icon/favicon.ico"/>';
      html += '<link rel="stylesheet" href="/style">';
      html += "<title>cakeu's photos</title></head>";
      html += '<body><br><h1>photo gallery</h1><br><br><br>';
@@ -66,7 +92,7 @@ function pageGallery(){
      for (let f of sFiles) {
           let img = f.slice(0, f.length-10);
           let href = "/imageviewer/" + img + ".jpg";
-          html += '<a href="' + href + '"><img src="/image/' + f + '"></a> ';
+          html += '<a class="photoGalleryElement" href="' + href + '"><img src="/image/' + f + '"></a> ';
      }
 
      html += '</div><br><br><br>';
@@ -76,33 +102,72 @@ function pageGallery(){
      return html;
 }
 
+// Visualisateur d'image (url: .../imageviewer/imageXYZ.jpg)
+// Affiche des boutons pour passer à l'image précedente ou suivante
+// Possibilité de cliquer l'image pour la passer en plein écran/la télécharger
+// Possibilité de déposer des commentaires et de les visualiser
 function pageViewer(page){
-     const imageCount = 37;
-
      if(page < 1 || page > imageCount)
           return page404("image not found");
 
      let html = "<!DOCTYPE html><html lang='fr'>";
      html += "<meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'>";
      html += "<link rel='stylesheet' href='/style'>";
+     html += '<link rel="icon" href="image/icon/favicon.ico"/>';
      html += "<title>cakeu's photos</title></head>";
-     html += "<body><br><h1>photo viewer</h1><br>";
+     html += "<body><br><h1>photo viewer</h1>";
+     html += "<p>(" + page + "/" + imageCount + ")<br><br><br>";
+
+
+     // photo viewer
      html += "<div class='photoViewer'>";
-     html += "<img src='/image/image" + page + ".jpg' id='largeImage'><br><br>";
-     if(page >= 1) 
+     html += "<a href='/image/image" + page + ".jpg' id='largeImage'><img src='/image/image" + page + ".jpg' id='largeImage'></a><br><br>";
+     if(page > 1) 
           html += "<a href='/imageviewer/image" + (parseInt(page)-1) + ".jpg' id='navPhotoLeft'> <img src='/image/image" + (parseInt(page)-1) + "_small.jpg'></a>";
      if(page < imageCount)
           html += "<a href='/imageviewer/image" + (parseInt(page)+1) + ".jpg' id='navPhotoRight'> <img src='/image/image" + (parseInt(page)+1) + "_small.jpg'></a>";
      html += "</div>";
-     html += "<a href='/gallery'>return</a>";
-     html += '</body></html>';
+     html += "<a href='/gallery'>return</a><br><br>";
+
+
+     html += "<h2>comments:</h2>";
+     // comment box
+     html += '<form action="/description-image" method="post">';
+     html += '<input type="hidden" name="image-number" value=' + page +'>';
+     html += '<input type="text" name="description">';
+     html += '<input type="submit" value="Envoyer"></form><br>';
+
+     // show comments
+     if(descriptions[page] == ""){
+          html += "<p> no comments yet.<br>be the first to make one!</p>"
+     }
+     else{
+          html += descriptions[page];
+     }
+     html += '<br><br></body></html>';
 
      return html;
 }
 
+// Inutilisé depuis le passage à la rédirection directe après publication de message
+function pageDescImage(msg, img){
+     let html = "<!DOCTYPE html><html lang='fr'>";
+     html += "<head><link rel='stylesheet' href='/style'>";
+     html += '<link rel="icon" href="image/icon/favicon.ico"/>';
+     html += "<title>cakeu's photos</title></head>";
+     html += "<br><h1>comment</h1>";
+     html += "<p>your comment <b>'" + msg + "'</b> has been sent successfully!</p>";
+     href = "/imageviewer/image" + img + ".jpg";
+     html += "<br><a href='" + href + "'>view</a>";
+     
+     return html;
+}
+
+// Affiche une page404 dans le cas d'une URL incorrecte
 function page404(msg){
      let html = "<!DOCTYPE html><html lang='fr'>";
      html += "<head><link rel='stylesheet' href='/style'>";
+     html += '<link rel="icon" href="image/icon/favicon.ico"/>';
      html += "<title>cakeu's photos</title></head>";
      html += "<br><h1>error 404</h1>";
      html += "<p>" + msg + " :v</p>"
@@ -112,6 +177,7 @@ function page404(msg){
      return html;
 }
 
+// Log les erreurs de manière consistante dans la console
 function errorHandler(err){
      console.log("(!) ERROR: " + err);
 }
